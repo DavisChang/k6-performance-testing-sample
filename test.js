5a1db26524815c340ea0e40f64149587a6637797
Davis Chang <davis.chang@langlive.com>
2021-09-22 14:48:14 +0800

    use group

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import http from 'k6/http'
 import { Rate } from 'k6/metrics'
-import { check } from 'k6'
+import { check, group } from 'k6'
 
 // new threshold without any error
 const failures = new Rate('faild requests')
@@ -16,10 +16,24 @@ export const options = {
 }
 
 export default function() {
-  const result = http.get('https://test-api.k6.io/')
-  check(result, {
-    'http response status code is 200': r => r.status === 200,
-  })
 
-  failures.add(result.status !== 200)
+  group('visit https://test-api.k6.io/', function () {
+    const result = http.get('https://test-api.k6.io/')
+    // const result = http.get('https://www.poponote.com/')
+    check(result, {
+      'http response status code is 200': r => r.status === 200,
+    })
+
+    failures.add(result.status !== 200)
+  });
+
+  group('visit https://test-api.k6.io/public/crocodiles/', function () {
+    const result2 = http.get('https://test-api.k6.io/public/crocodiles/')
+    check(result2, {
+      'http response status code is 200': r => r.status === 200,
+    })
+
+    failures.add(result2.status !== 200)
+  });
+
 }
